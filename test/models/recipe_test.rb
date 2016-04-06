b2965d4defe212f70c1a8b1fb693299fd76fd35d
Miguel Saldana <mig.saldana@gmail.com>
2016-04-05 21:15:26 -0500

    created chef and recipes association and tests

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,12 +3,28 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
   def setup
-    @recipe = Recipe.new(name: "chicken parm", summary: "Thi is the best chicken parm recipe ever",
-              description: "heat oil, add onions, add tomato sauce, add chicken, cook 20 minutes")
+
+    #@chef = Chef.new(chefname: "bob", email: "bob@example.com")
+    # once association of recipe with chef is made this wont help us because .new is on memory but not in database
+    # we should use create to make it be on database
+    @chef = Chef.create(chefname: "bob", email: "bob@example.com")
+    @recipe = @chef.recipes.build(name: "chicken parm", summary: "Thi is the best chicken parm recipe ever",
+             description: "heat oil, add onions, add tomato sauce, add chicken, cook 20 minutes")
+
+    # code below was used to test chef without any associations, was modified above because the relationship of chef and recipes
+    #was created
+    #
+    #@recipe = Recipe.new(name: "chicken parm", summary: "Thi is the best chicken parm recipe ever",
+    #         description: "heat oil, add onions, add tomato sauce, add chicken, cook 20 minutes")
   end
 
   test "recipe should be valid" do
     assert @recipe.valid?
+  end
+
+  test "chef_id should be present" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
   end
 
   test "name should be present" do
