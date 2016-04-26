da770c094e35cde7167a2f61457fe7827be00a72
Miguel Saldana <mig.saldana@gmail.com>
2016-04-25 20:23:41 -0500

    Added admin functionality

--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,8 @@ Rails.application.routes.draw do
       post 'like'
     end
   end
-  #this line creates all of below
-  #get '/recipes', to: 'recipes#index'
-  #get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
-  #post '/recipes', to: 'recipes#create'
-  #this is to handle the submission of form. whenever we create we do post request
-  #get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
-  ##this will render a form because we are doing a submission so we need to use patch because is an edit
-  #patch '/recipes/:id', to: 'recipes#update'
-  #get 'recipes/:id', to: 'recipes#show', as: 'recipe'
-  #delete '/recipes/:id', to: 'recipes#destroy'
 
-  resources :chefs, except: [:new]
+  resources :chefs, except: [:new, :destroy]
 
   get '/register', to: 'chefs#new'
 
